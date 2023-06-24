37131284127d21af2d6d616c34693e1f4740b307
cherelemma <chere.lemma@aastu.edu.et>
2023-06-24 22:05:17 +0300

    validate author name and posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
     has_many :comments, foreign_key: :user_id, dependent: :destroy
     has_many :likes, foreign_key: :user_id, dependent: :destroy
   
-    def recent_posts(limit = 3)
+    def recent_posts(limit = 1)
       posts.order(created_at: :desc).limit(limit)
     end
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :posts_counter, numericality: { only_integer: true,  greater_than_or_equal_to: 0 },
   end
   
