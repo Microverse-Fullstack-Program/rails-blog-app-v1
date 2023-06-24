4b07df77731b0796fa25a20f4aa401fb52afab18
cherelemma <chere.lemma@aastu.edu.et>
2023-06-24 21:22:30 +0300

    update post model class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  after_save :update_post_counter
+  after_destroy :update_post_counter
 
   def update_post_counter
     author.update(posts_counter: author.posts.count)
