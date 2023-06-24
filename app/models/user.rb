444bfd9bb613310f4729eee95f3ea5780b5ad2cd
cherelemma <chere.lemma@aastu.edu.et>
2023-06-24 21:21:34 +0300

    update user model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-
-  def recent_posts(limit = 3)
-    posts.order(created_at: :desc).limit(limit)
+    has_many :posts, foreign_key: :author_id, dependent: :destroy
+    has_many :comments, foreign_key: :user_id, dependent: :destroy
+    has_many :likes, foreign_key: :user_id, dependent: :destroy
+  
+    def recent_posts(limit = 3)
+      posts.order(created_at: :desc).limit(limit)
+    end
   end
-end
+  
