a6d06f03f44b1b3b51d99ca4cb510ce1533d8912
cherelemma <chere.lemma@aastu.edu.et>
2023-06-29 20:17:12 +0300

    add action to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+      end
+    
+      def show
+        @posts = Post.find_by(id: params[:id])
+      end
 end
