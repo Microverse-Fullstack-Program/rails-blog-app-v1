f23a02d297c1d9784e1466785785db1ecdc451d5
cherelemma <chere.lemma@aastu.edu.et>
2023-07-02 00:37:53 +0300

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,34 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def like
+    @post = Post.find(params[:id])
+    @like = Like.new(author_id: params[:user_id], post_id: @post.id)
+    @like.save
+    redirect_to user_post_path
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counters = 0
+
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
