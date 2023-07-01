5f16a2b4d00178aab098cf211ad287f06b8b19af
cherelemma <chere.lemma@aastu.edu.et>
2023-07-01 21:17:02 +0300

    add data logic to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find_by_id(params[:id])
+
+    if @user.nil?
+      redirect_to users_path
+    end
+    @posts = @user.posts
+  end
 end
