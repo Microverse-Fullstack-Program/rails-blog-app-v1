8bfb8195ed4f95cc6340b74ca82aceed1eb9ff13
cherelemma <chere.lemma@aastu.edu.et>
2023-07-15 17:24:40 +0300

    fix linter errors

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::PostsController < ApplicationController
   def index
     user = User.find(params[:user_id])
     posts = user.posts
-    
+
     if posts.present?
       render json: { message: 'Posts loaded successfully', data: posts }, status: :ok
     else
